970e4b69905bd2c81479b371547d4c2df6bb4baf
下田　佳紀 <y.s@experience.local>
2020-01-02 07:28:04 +0900

    20200102

--- db/migrate/20200101170529_create_microposts.rb
+++ db/migrate/20200101170529_create_microposts.rb
@@ -1,10 +1,12 @@
 class CreateMicroposts < ActiveRecord::Migration[6.0]
   def change
+    drop_table :microposts
     create_table :microposts do |t|
       t.text :content
-      t.integer :user_id
+      t.references :user, null: false, foreign_key: true
 
       t.timestamps
     end
+    add_index :microposts, [:user_id, :created_at]
   end
 end
