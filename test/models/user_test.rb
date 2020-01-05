f8425e2ede4f076f8559182172066121737849eb
下田　佳紀 <y.s@experience.local>
2020-01-05 14:20:02 +0900

    20200105a

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -81,7 +81,18 @@ class UserTest < ActiveSupport::TestCase
     @user.save
     @user.microposts.create!(content: "Lorem ipsum")
     assert_difference 'Micropost.count', -1 do
-      @user.destroy
+    @user.destroy
+  end
+    
+  test "should follow and unfollow a user" do
+    michael  = users(:michael)
+    archer   = users(:archer)
+    assert_not michael.following?(archer)
+    michael.follow(archer)
+    assert michael.following?(archer)
+    assert archer.followers.include?(michael)
+    michael.unfollow(archer)
+    assert_not michael.following?(archer)
     end
   end
 end
